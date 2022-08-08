21e53553b68a4bf08ebdd8afab095bd5878b145c
Vhulenda88 <u19050993@tuks.co.za>
2022-08-08 16:00:30 +0200

    Routing

--- web/home.js
+++ web/home.js
@@ -3,3 +3,11 @@
 function info() {
     window.location.href = "infoCenter.html";
 }
+
+function health() {
+    window.location.href = "HealthCenter/index.html";
+}
+
+function ask() {
+    window.location.href = "AskMyPeers/index.html";
+}
